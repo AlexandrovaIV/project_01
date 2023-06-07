563181e48f69db43d7f8f3d1187242d3c178fcfb
irina alexandrova <irina.sib@mail.ru>
2023-06-07 12:10:49 +0700

    add file task_1.1.py

---
+++ Homeworks/lvl_1/task_1.1.py
@@ -0,0 +1,3 @@
+# Задача 1.1.
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+print(my_favorite_songs[:14], my_favorite_songs[64:], my_favorite_songs[16:30], my_favorite_songs[-26:-15])
